195b31c539702a3fdd2b7e66ef4baa832eb24f4d
stanno03 <danno_41@hotmail.com>
2023-04-12 20:32:13 +1000

    added HTML, CSS and JS

---
+++ js/script.js
@@ -0,0 +1,41 @@
+var startButton = $("#btn btn-primary");
+
+var questions = [
+    {
+		question: "What is 10/2?",
+			Option1: '3',
+			Option2: '5',
+			Option3: '115',
+			Option4: '60',
+			correctAnswer: 'b'
+		},
+
+	{
+		question: "What is 30/3?",
+		Choice1: '3',
+		Option2: '5',
+		Option3: '10',
+		Option4: '8',
+			correctAnswer: 'c'
+		},
+	{	
+		question: "What HTML tag is used to embed a script",
+		Choice1: '3',
+		Option2: '5',
+		Option3: '10',
+		Option4: '8',
+			correctAnswer: 'c'
+		},
+   
+];
+
+function startTimer()
+
+function displayQuestion(){
+
+};
+
+function startGame();
+
+
+// startButton.on('click', startGame());
